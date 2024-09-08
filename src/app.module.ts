ea339a2518a2ee08fc61a4f877f6f60a6d91145e
AlexIvantsovHW <alexander.ivantsov.hw@gmail.com>
2024-09-08 12:32:10 +0300

    feat: create jobs endpont

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { JobModule } from './job/job.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+
+import { GraphQLModule } from "@nestjs/graphql";
+import { join } from "path";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { JobModule } from "./job/job.module";
 
 @Module({
-  imports: [JobModule],
+  imports: [
+    JobModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true, // or path to your schema file if you have one
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/job/job.resolver.ts
+++ src/job/job.resolver.ts
@@ -1,35 +1,20 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { JobService } from './job.service';
-import { Job } from './entities/job.entity';
-import { CreateJobInput } from './dto/create-job.input';
-import { UpdateJobInput } from './dto/update-job.input';
-
+import { Job } from "./entities/job.entitites";
+import { Resolver, Query } from "@nestjs/graphql";
 @Resolver(() => Job)
 export class JobResolver {
-  constructor(private readonly jobService: JobService) {}
-
-  @Mutation(() => Job)
-  createJob(@Args('createJobInput') createJobInput: CreateJobInput) {
-    return this.jobService.create(createJobInput);
-  }
-
-  @Query(() => [Job], { name: 'job' })
-  findAll() {
-    return this.jobService.findAll();
-  }
-
-  @Query(() => Job, { name: 'job' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.jobService.findOne(id);
-  }
-
-  @Mutation(() => Job)
-  updateJob(@Args('updateJobInput') updateJobInput: UpdateJobInput) {
-    return this.jobService.update(updateJobInput.id, updateJobInput);
-  }
-
-  @Mutation(() => Job)
-  removeJob(@Args('id', { type: () => Int }) id: number) {
-    return this.jobService.remove(id);
+  @Query(() => [Job], { name: "jobs" })
+  getJobs(): Job[] {
+    return [
+      {
+        id: 1,
+        title: "Software Developer",
+        description: "Develops software solutions.",
+      },
+      {
+        id: 2,
+        title: "Project Manager",
+        description: "Manages project timelines and deliverables.",
+      },
+    ];
   }
 }
